c2b4cfd92d037d9a607b252d80c6ea90b8af8cb4
Aaron Shaver <AaronShaverPDX@gmail.com>
2016-04-01 16:23:27 -0700

    More documentation, horizontal alignment function

--- script.js
+++ script.js
@@ -2,6 +2,20 @@
 // RepairFormat for Google Sheets 1.0
 // ----------------------------------
 //
+// INSTALLATION:
+// 
+// If you haven't installed this as an add-on, you can use it by clicking
+// 'Tools' > 'Script editor' in Sheets and then pasting the code into the
+// Code.gs document. You might then need to "bind" the script to the sheet
+// you're working with.
+//12345678901234567890123456789012345678901234567890123456789012345678901234567|
+//12345678901234567890123456789012345678901234567890123456789012345678901234567|
+//12345678901234567890123456789012345678901234567890123456789012345678901234567|
+//12345678901234567890123456789012345678901234567890123456789012345678901234567|
+//12345678901234567890123456789012345678901234567890123456789012345678901234567|
+//
+// USAGE:
+//
 // When you click on the add-on's menu item, it will perform the following
 // operations on every part of the sheet which has content:
 //
@@ -9,15 +23,16 @@
 // 2. Set 'Font size' to a consistent value across all content
 // 3. Auto-resize all columns to fit their content
 // 4. Set 'Horizontal align' to 'Left'
-
-//12345678901234567890123456789012345678901234567890123456789012345678901234567|
+//
 
 fontSize = 14;
+alignment = "left";
 
 function repairFormat() {
   var sheet = SpreadsheetApp.getActiveSheet();
   clearFormats(sheet);
   increaseFontSize(sheet);
+  horizontallyAlign(sheet);
   autoResizeColumns(sheet);
 }
 
@@ -27,6 +42,10 @@ function clearFormats(sheet) {
 
 function increaseFontSize(sheet) {
   sheet.getDataRange().setFontSize(fontSize);
+};
+
+function horizontallyAlign(sheet) {
+  sheet.getDataRange().setHorizontalAlignment(alignment);
 };
 
 function autoResizeColumns(sheet) {
