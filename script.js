cd68c21cd739a5b379df31c46ad53e195f58cbb8
Aaron Shaver <AaronShaverPDX@gmail.com>
2016-04-01 17:16:16 -0700

    Removed documentation from script; start of new header vs sheet preferences code

--- script.js
+++ script.js
@@ -1,52 +1,41 @@
-// ==================================
-// RepairFormat for Google Sheets 1.0
-// ==================================
-//
-// INSTALLATION
-// ------------
-// 
-// If you haven't installed this as an add-on, you can use it by clicking
-// 'Tools' > 'Script editor' in Sheets and then pasting the code into the
-// Code.gs document. You might then need to "bind" the script to the sheet
-// you're working with.
-//
-// USAGE
-// -----
-//
-// When you click on the add-on's menu item, it will perform the following
-// operations on every part of the sheet which has content:
-//
-// 1. Clears formatting for text (bold, italics, text color, etc.)
-// 2. Sets 'Font size'
-// 3. Sets 'Font' (font family)
-// 4. Sets 'Horizontal align'
-// 5. Auto-resizes all columns to fit their content (has a slight delay)
-
-var preferences = {
+var sheetPreferences = {
   fontSize: 14,
   alignment: "left",
-  fontFamily: "Consolas"
+  fontFamily: "Consolas",
+  skipHeaderRow: true
+};
+
+var sheetPreferences = {
+  fontSize: 14,
+  alignment: "left",
+  fontFamily: "Consolas",
+  skipHeaderRow: true
 };
 
 function repairFormat() {
   var sheet = SpreadsheetApp.getActiveSheet();
-  sheet.clearFormats();
+  clearFormats(sheet);
   setFontSize(sheet);
   setFontFamily(sheet);
   setHorizontalAlignment(sheet);
   autoResizeColumns(sheet);
+  formatHeaderRow();
+}
+
+function clearFormats(sheet) {
+    sheet.clearFormats();
 }
 
 function setFontSize(sheet) {
-  sheet.getDataRange().setFontSize(preferences.fontSize);
+  sheet.getDataRange().setFontSize(sheetPreferences.fontSize);
 };
 
 function setFontFamily(sheet) {
-  sheet.getDataRange().setFontFamily(preferences.fontFamily);
+  sheet.getDataRange().setFontFamily(sheetPreferences.fontFamily);
 };
 
 function setHorizontalAlignment(sheet) {
-  sheet.getDataRange().setHorizontalAlignment(preferences.alignment);
+  sheet.getDataRange().setHorizontalAlignment(sheetPreferences.alignment);
 };
 
 function autoResizeColumns(sheet) {
